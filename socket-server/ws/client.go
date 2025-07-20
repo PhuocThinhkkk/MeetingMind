6a0547f0ea50691ba89798e571bb773ef22db511
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-07-20 21:07:46 +0700

    keep track on current state

--- socket-server/ws/client.go
+++ socket-server/ws/client.go
@@ -1,25 +1,79 @@
 package ws
 
 import (
-	"log"
 	"encoding/json"
-    "github.com/gorilla/websocket"
+	"errors"
 	"fmt"
+	"log"
+	"slices"
+	"strings"
+	"github.com/gorilla/websocket"
 )
 
 type Client struct {
 	Conn *websocket.Conn
-	Text  chan []byte
 	AssemblyConn  *websocket.Conn
 	Done   chan struct{}
+	Transcript *TranscriptState
 }
+
+type TranscriptState struct {
+	
+	WordsTranscript []string
+	CurrentSentence    []string 
+	PartialWord   []string 
+	CurrentTurnID int   
+
+}
+
+func (c *Client) addWordsTranscript(t string){
+	c.Transcript.WordsTranscript = append(c.Transcript.WordsTranscript, t)
+}
+
+func (c *Client) setCurrentSentence(t string){
+	h := make([]string, 0)
+	h = append(h, t)
+	c.Transcript.CurrentSentence = h 
+}
+
+func (c *Client) setPartialWord(t string){
+	h := make([]string, 0)
+	h = append(h, t)
+	c.Transcript.PartialWord = h
+}
+
+type AssemblyResponseWord struct {
+    Start       int     `json:"start"`
+    End         int     `json:"end"`
+    Text        string  `json:"text"`
+    Confidence  float64 `json:"confidence"`
+    WordIsFinal bool    `json:"word_is_final"`
+}
+
+type AssemblyRessponseTurn struct {
+    TurnOrder          int     `json:"turn_order"`
+    Transcript         string  `json:"transcript"`
+    EndOfTurn          bool    `json:"end_of_turn"`
+    EndOfTurnConfidence float64 `json:"end_of_turn_confidence"`
+    Words              []AssemblyResponseWord  `json:"words"`
+    Type               string  `json:"type"`
+}
+
 
 func NewClient (Conn *websocket.Conn, AssemblyConn *websocket.Conn) *Client {
 	return &Client {
 		Conn: Conn,
-		Text: make(chan []byte),
 		AssemblyConn: AssemblyConn,
 		Done: make(chan struct{}),
+		Transcript: NewTranscriptState(),	
+	}
+}
+
+func NewTranscriptState() *TranscriptState {
+	return &TranscriptState {
+		CurrentSentence : make([]string, 0),
+		PartialWord: make([]string, 0),
+		CurrentTurnID: -1,
 	}
 }
 
@@ -107,5 +161,35 @@ func (c *Client) writeText() {
 func UnregisterClient(c *Client) {
 	close(c.Done)
     c.Conn.Close()
-    close(c.Text)
 }
+
+func (c *Client) updateStateTranscript(jsonData []byte) error {
+	var turn AssemblyRessponseTurn
+	err := json.Unmarshal(jsonData, &turn)
+	if err != nil {
+		err1 := errors.New("cant parse json from Assembly: ")
+		joinErr := errors.Join(err1, err)
+		return joinErr
+	}
+
+	if turn.EndOfTurn {
+		c.addWordsTranscript(turn.Transcript)
+	}
+		
+	c.setCurrentSentence(turn.Transcript)
+
+	for _, word := range turn.Words {
+		if !word.WordIsFinal {
+			c.setPartialWord(word.Text)
+		}
+	}
+	return nil
+
+}
+
+func (c *Client) save() {
+	// handle save all words to db
+}
+
+
+ 
