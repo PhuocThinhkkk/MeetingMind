b89c2de994918d638948dfe68b76e32202122c20
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-10-22 09:32:19 +0000

    fix: remove custom alias on the worklet processer
    worklets run in a sandboxed browser environment without module alias
    support so it make an error if we include import with alias

--- public/worklet-processor.js
+++ public/worklet-processor.js
@@ -1,32 +1,36 @@
-import { log } from "@/lib/logger";
 class PCMProcessor extends AudioWorkletProcessor {
-    process(inputs) {
-        const input = inputs[0];
-        if (!input || input.length === 0) return true;
+  process(inputs) {
+    try {
+      const input = inputs[0];
+      if (!input || input.length === 0) return true;
 
-        const channelCount = input.length;
-        if (channelCount > 1 ) {
-            log.info("channelCount: ", channelCount)
+      const channelCount = input.length;
+      if (channelCount > 1) {
+        console.log("[INFOR] channelCount: ", channelCount);
+      }
+
+      let mixed;
+      if (channelCount === 1) {
+        mixed = input[0];
+      } else {
+        const length = input[0].length;
+        mixed = new Float32Array(length);
+        for (let i = 0; i < length; i++) {
+          let sum = 0;
+          for (let ch = 0; ch < channelCount; ch++) {
+            sum += input[ch][i];
+          }
+          mixed[i] = sum / channelCount;
         }
+      }
 
-        let mixed;
-        if (channelCount === 1) {
-            mixed = input[0]; 
-        } else {
-            const length = input[0].length;
-            mixed = new Float32Array(length);
-            for (let i = 0; i < length; i++) {
-                let sum = 0;
-                for (let ch = 0; ch < channelCount; ch++) {
-                    sum += input[ch][i];
-                }
-                mixed[i] = sum / channelCount;
-            }
-        }
-
-        this.port.postMessage(mixed);
-        return true;
+      this.port.postMessage(mixed);
+      return true;
+    } catch (error) {
+      console.error("[ERROR] Error in PCMProcessor:", error);
+      return true;
     }
+  }
 }
 
 registerProcessor("pcm-processor", PCMProcessor);
