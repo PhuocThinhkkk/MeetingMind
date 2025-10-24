d0f039c82b47fd61917a4810bef5feceac95cd6e
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2025-10-24 07:07:15 +0000

    fix(logger): handling custom log on the browser

--- lib/logger.ts
+++ lib/logger.ts
@@ -1,12 +1,32 @@
 import chalk from "chalk";
 
 export type LogContext = unknown;
+const isServer = typeof window === "undefined";
+
+const browserStyles = {
+  info: "color: dodgerblue;",
+  warn: "color: orange;",
+  error: "color: red;",
+};
 
 export const log = {
-  info: (msg: string, ctx?: LogContext) =>
-    console.log(chalk.blue(`[INFO] ${msg}`), ctx ?? ""),
-  warn: (msg: string, ctx?: LogContext) =>
-    console.warn(chalk.yellow(`[WARN] ${msg}`), ctx ?? ""),
-  error: (msg: string, ctx?: LogContext) =>
-    console.error(chalk.red(`[ERROR] ${msg}`), ctx ?? ""),
+  info: (msg: string, ctx?: LogContext) => {
+    if (isServer)
+      console.log(chalk.blue(`[INFO] ${msg}`), ctx ?? "");
+    else
+      console.log(`%c[INFO] ${msg}`, browserStyles.info, ctx ?? "");
+  },
+  warn: (msg: string, ctx?: LogContext) => {
+    if (isServer)
+      console.warn(chalk.yellow(`[WARN] ${msg}`), ctx ?? "");
+    else
+      console.warn(`%c[WARN] ${msg}`, browserStyles.warn, ctx ?? "");
+  },
+  error: (msg: string, ctx?: LogContext) => {
+    if (isServer)
+      console.error(chalk.red(`[ERROR] ${msg}`), ctx ?? "");
+    else
+      console.error(`%c[ERROR] ${msg}`, browserStyles.error, ctx ?? "");
+  },
 };
+
