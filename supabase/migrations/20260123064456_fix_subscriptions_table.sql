051b683e2ded63e1b0d1a1e5931c9915351ee39e
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2026-01-23 14:38:51 +0700

    migration: appling fix migration subscription table to the cloud (I can't revert all the change on the cloud )

---
+++ supabase/migrations/20260123064456_fix_subscriptions_table.sql
@@ -0,0 +1,41 @@
+-- I fk up the previous migration about this table and it didnt apply, so I create this one again
+create table public.subscriptions (
+  id uuid primary key default gen_random_uuid(),
+
+  user_id uuid not null unique
+    references auth.users(id) on delete cascade,
+
+  stripe_customer_id text not null unique,
+  stripe_subscription_id text not null unique,
+
+  status text not null
+    check (status in ('active', 'trialing', 'past_due', 'canceled')),
+
+  price_id text not null,
+  current_period_end timestamptz not null,
+
+  cancel_at_period_end boolean not null default false,
+
+  created_at timestamptz not null default now(),
+  updated_at timestamptz not null default now()
+);
+
+create or replace function public.set_updated_at()
+returns trigger as $$
+begin
+  new.updated_at = now();
+  return new;
+end;
+$$ language plpgsql;
+
+create trigger set_subscriptions_updated_at
+before update on public.subscriptions
+for each row
+execute procedure public.set_updated_at();
+
+alter table public.subscriptions enable row level security;
+
+create policy "Users can read own subscription"
+on public.subscriptions
+for select
+using (auth.uid() = user_id);
