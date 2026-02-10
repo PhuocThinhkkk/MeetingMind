5f7e8d1fea8d1cc1c6d43159ec2c394b4cbcb01a
PhuocThinhkkk <nguyenphuocthinh10c2@gmail.com>
2026-02-10 19:56:23 +0700

    migration: update private storage

---
+++ app/api/audiofile/url-upload/route.ts
@@ -0,0 +1,76 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { getUserAuthInSupabaseToken } from '@/lib/supabase-auth-server'
+import { log } from '@/lib/logger'
+import { validateAudioFile } from '@/services/audio-upload/audio-validation'
+import {
+  insertAudioFile,
+  uploadAudioFile,
+} from '@/lib/queries/server/audio-upload-operations'
+import { createAssemblyAudioUploadWithWebhook } from '@/services/audio-upload/assembly-webhook'
+import { getAudioDuration } from '@/lib/transcript/transcript-realtime-utils'
+import {
+  getMonthlyUploadCount,
+  getMonthlyUsageSeconds,
+  getUserPlan,
+} from '@/lib/queries/server/limits-audio-upload-operations'
+import { checkTranscriptionAllowed } from '@/lib/limits/usage.limit'
+
+export async function POST(req: NextRequest) {
+  try {
+    const user = await getUserAuthInSupabaseToken()
+    if (!user?.id) {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+    }
+
+    const { name, duration, size, type } = await req.json()
+
+    const [totalSeconds, uploadsCount] = await Promise.all([
+      getMonthlyUsageSeconds(user.id),
+      getMonthlyUploadCount(user.id),
+    ])
+
+    const userPlan = await getUserPlan(user.id)
+    const { allowed, reason } = checkTranscriptionAllowed({
+      plan: userPlan,
+      usedSeconds: totalSeconds,
+      fileSeconds: duration,
+    })
+    if (!allowed) {
+      return NextResponse.json(
+        {
+          error: reason,
+        },
+        { status: 500 }
+      )
+    }
+
+    const audioUrl = await uploadAudioFile(user.id, file)
+    const job = await createAssemblyAudioUploadWithWebhook(audioUrl)
+    log.info('Jobs of uploading file: ', job)
+    const dataInsert = {
+      user_id: user.id,
+      name: name,
+      file_size: size,
+      duration,
+      mine_tyep: type,
+      url: audioUrl,
+      assembly_job_id: job.id,
+      transcription_status: 'processing',
+    }
+    const audio = await insertAudioFile(dataInsert)
+
+    return NextResponse.json({
+      audio_id: audio.id,
+      audio,
+      status: 'processing',
+    })
+  } catch (e) {
+    log.error('Error in the upload server: ', e)
+    return NextResponse.json(
+      {
+        error: 'Servr Error',
+      },
+      { status: 500 }
+    )
+  }
+}
